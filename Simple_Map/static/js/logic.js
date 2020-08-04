1adb8369e24e7becb68957180c57cc49c2773fc6
Rob <rlau929@gmail.com>
2020-08-03 19:37:13 -0700

    Adding logic.js file

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -3,7 +3,7 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
-// // ALTERNATIVE: Create the map object with a center and zoom level.
+// // ALTERNATIVE to setView: Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
 //     center: [
 //       40.7, -94.5
