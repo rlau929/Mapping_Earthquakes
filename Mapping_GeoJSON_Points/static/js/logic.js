9519b6fda3ad1fe5e811703254542e027404b770
Rob <rlau929@gmail.com>
2020-08-05 17:49:24 -0700

    Adding logic.js

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,49 +1,8 @@
  // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-// // Grabbing our GeoJSON data. pointToLayer
-// L.geoJson(sanFranAirport, {
-//     // We turn each feature into a marker on the map.
-//     pointToLayer: function(feature, latlng) {
-//       console.log(feature);
-// 	  return L.marker(latlng)
-// 	  .bindPopup("<h2>" + feature.properties.city + "</h2>")
-//     }
-
-//   }).addTo(map);
-
-// Grabbing our GeoJSON data. onEachFeature
-L.geoJson(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    onEachFeature: function(feature, layer) {
-	  console.log(layer);
-	  layer.bindPopup();
-    }
-
-  }).addTo(map);
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -55,15 +14,12 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// Get data from cities.js
-let cityData = cities;
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/rlau929/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-	console.log(city)
-	L.circleMarker(city.location, {
-		radius: city.population/100000
-	})
-	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
 });
